3e5392b616e48c3d96f19b125f01acbd888a01e2
SeongUk18 <108208136+SeongUk18@users.noreply.github.com>
2024-01-18 14:21:07 +0900

    Title: 대응되는 수와 문자, Time: 827ms, Memory: 55MB, Status: Passed - CodeTree

--- 240118/대응되는 수와 문자/corresponding-numbers-and-characters.py
+++ 240118/대응되는 수와 문자/corresponding-numbers-and-characters.py
@@ -1,14 +1,16 @@
 n, m = map(int, input().split())
 
-dic = {}
+dic1 = {}
+dic2 = {}
 
 for i in range(1, n + 1):
     val = input()
-    dic[str(i)] = val
+    dic1[str(i)] = val
+    dic2[val] = str(i)
 
 for _ in range(m):
     find_word = input()
-    if find_word in dic:
-        print(dic[find_word])
+    if find_word in dic1:
+        print(dic1[find_word])
     else:
-        print(list(dic.values()).index(find_word) + 1)
+        print(dic2[find_word])
