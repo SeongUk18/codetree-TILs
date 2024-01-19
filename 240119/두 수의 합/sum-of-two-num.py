034db909acbbebd247d0cb34a901eedfeaa15015
SeongUk18 <108208136+SeongUk18@users.noreply.github.com>
2024-01-19 17:01:17 +0900

    Title: 두 수의 합, Time: 186ms, Memory: 36MB, Status: Passed - CodeTree

--- 240119/두 수의 합/sum-of-two-num.py
+++ 240119/두 수의 합/sum-of-two-num.py
@@ -1,15 +1,36 @@
-from itertools import combinations
+# 조합으로 풀이하려고 했지만 시간 초과로 풀이 방법 변경
+# from itertools import combinations
+
+# n, m = map(int, input().split())
+
+# numbers = list(map(int, input().split()))
+# num = 0
+# num_list = []
+
+# for i in combinations(numbers, 2):
+#     if i in num_list:
+#         num += 1
+#     elif sum(i) == m:
+#         num += 1
+
+# print(num)
 
 n, m = map(int, input().split())
 
 numbers = list(map(int, input().split()))
-num = 0
-num_list = []
 
-for i in combinations(numbers, 2):
-    if i in num_list:
-        num += 1
-    elif sum(i) == m:
-        num += 1
+dic = {}
+answer = 0
 
-print(num)
+for i in numbers:
+    target = m - i
+
+    if target in dic:
+        answer += dic[target]
+
+    if i in dic:
+        dic[i] += 1
+    else:
+        dic[i] = 1
+
+print(answer)
