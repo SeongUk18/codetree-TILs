92aa02a06470020f66562534d59e7bb3ac43e12f
SeongUk18 <108208136+SeongUk18@users.noreply.github.com>
2024-01-19 18:26:29 +0900

    Title: 세 수의 합, Time: 500ms, Memory: 98MB, Status: Passed - CodeTree

--- 240119/세 수의 합/sum-of-three-num.py
+++ 240119/세 수의 합/sum-of-three-num.py
@@ -7,9 +7,21 @@ numbers = list(map(int, input().split()))
 dic = {}
 answer = 0
 
-for i in combinations(numbers, 3):
-    if sum(i) == m:
-        answer += 1
+for i in range(n):
+    for j in range(1 + i, n):
+        sum_num = numbers[i] + numbers[j]
+        if sum_num in dic:
+            dic[sum_num].append((i, j))
+        else:
+            dic[sum_num] = [(i, j)]
 
+for i in range(n):
+    target = m - numbers[i]
+    if target in dic:
+        for j in dic[target]:
+            if i not in j:
+                answer += 1
+
+answer = answer // 3
 
 print(answer)
