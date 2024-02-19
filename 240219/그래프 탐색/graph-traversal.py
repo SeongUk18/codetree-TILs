aa3907f16a6b0cc252f57ed936c93c67e8590363
SeongUk18 <108208136+SeongUk18@users.noreply.github.com>
2024-02-19 21:24:26 +0900

    Title: 그래프 탐색, Time: 194ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240219/그래프 탐색/graph-traversal.py
+++ 240219/그래프 탐색/graph-traversal.py
@@ -1,10 +1,28 @@
+from collections import deque
+
 n, m = map(int, input().split())
 
 node_list = [[] for _ in range(n + 1)]
+node_chk = [False for _ in range(n + 1)]
+
 
 for _ in range(m):
     i, j = map(int, input().split())
     node_list[i].append(j)
     node_list[j].append(i)
 
-print(len(node_list[1]))
+def bfs(node):
+    q = deque()
+    q.append(node)
+    answer = 0
+    while q:
+        node = q.popleft()
+        node_chk[node] = True
+        for k in node_list[node]:
+            if not node_chk[k]:
+                q.append(k)
+                answer += 1
+    return answer
+answer = bfs(1)
+# 1번 간선을 제외하기 위한 -1
+print(answer-1)
