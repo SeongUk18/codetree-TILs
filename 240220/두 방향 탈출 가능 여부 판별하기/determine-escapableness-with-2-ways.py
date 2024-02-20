a1b74addda9fbcd4ee6e4f641eb593ef2d720122
SeongUk18 <108208136+SeongUk18@users.noreply.github.com>
2024-02-20 14:26:40 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 155ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240220/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+n, m = map(int, input().split())
+
+map_chk = [[False for _ in range(m)] for _ in range(n)]
+map_list = []
+change_y = [1, 0]
+change_x = [0, 1]
+
+for _ in range(n):
+    map_list.append(list(map(int, input().split())))
+
+def bfs(node):
+    x, y = node
+    q = deque()
+    q.append((y, x))
+    map_chk[y][x] = True
+    while q:
+        cur_y, cur_x = q.popleft()
+        for i in range(2):
+            next_y = cur_y + change_y[i]
+            next_x = cur_x + change_x[i]
+            if next_y > n - 1 or next_x > m - 1 or not map_chk[next_y][next_x] or map_list[next_y][next_x] == 0:
+                continue
+            elif next_x == n - 1 and next_y == m - 1:
+                return 1
+            else:
+                q.append((next_y,next_x))
+                map_chk[next_y][next_y] = True
+    return 0
+
+print(bfs((0, 0)))
