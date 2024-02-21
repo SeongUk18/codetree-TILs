3ea297aa56cd351ab5a0578434ec237158309bf3
SeongUk18 <108208136+SeongUk18@users.noreply.github.com>
2024-02-21 16:29:29 +0900

    Title: 처음 등장하는 위치, Time: 569ms, Memory: 39MB, Status: Wrong Answer - CodeTree

---
+++ 240221/처음 등장하는 위치/first-appearing-position.py
@@ -0,0 +1,15 @@
+from sortedcontainers import SortedDict 
+
+n = int(input())
+numbers = input().split()
+
+num_dic = SortedDict()
+
+for i in range(len(numbers)):
+    if numbers[i] in num_dic:
+        continue
+    else:
+        num_dic[numbers[i]] = i + 1
+
+for k, v in num_dic.items():
+    print("{} {}".format(k, v))
