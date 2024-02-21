c0df0a9604cb2f6cfd5236eb7b3adcf2c00f3e1c
SeongUk18 <108208136+SeongUk18@users.noreply.github.com>
2024-02-21 21:35:44 +0900

    Title: 정수 찾기, Time: 191ms, Memory: 36MB, Status: Passed - CodeTree

---
+++ 240221/정수 찾기/find-an-integer.py
@@ -0,0 +1,13 @@
+n = input()
+
+num_set = set(map(int, input().split()))
+
+m = input()
+
+num_list = list(map(int, input().split()))
+
+for i in num_list:
+    if i in num_set:
+        print(1)
+    else:
+        print(0)
