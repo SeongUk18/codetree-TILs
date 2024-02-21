fb230e9d30fcf7b8b549f7abc3ff8ae33326c4a6
SeongUk18 <108208136+SeongUk18@users.noreply.github.com>
2024-02-21 13:57:17 +0900

    Title: 특별한 문자, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240221/특별한 문자/special-character.py
@@ -0,0 +1,14 @@
+text = list(input())
+
+t_dic = {}
+
+
+for i in text:
+    if i in t_dic:
+        t_dic[i] += 1
+    else:
+        t_dic[i] = 1
+        
+n_dic = {v:k for k,v in t_dic.items()}
+
+print(n_dic.get(1))
