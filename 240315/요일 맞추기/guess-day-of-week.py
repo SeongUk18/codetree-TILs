77701788b30f6a626aca8420343771cba4f2f041
SeongUk18 <108208136+SeongUk18@users.noreply.github.com>
2024-03-15 15:45:01 +0900

    Title: 요일 맞추기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240315/요일 맞추기/guess-day-of-week.py
+++ 240315/요일 맞추기/guess-day-of-week.py
@@ -3,15 +3,27 @@ elapsed_days = 0
 day_of_the_week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 #                  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-if m1 != m2:
-    for i in range(m1, m2):
-        if i == m1:
-            elapsed_days += num_of_days[i] - d1
-        else:
-            elapsed_days += num_of_days[i]
-    elapsed_days += d2
+if m1 < m2 and d1 < d2:
+    if m1 != m2:
+        for i in range(m1, m2):
+            if i == m1:
+                elapsed_days += num_of_days[i] - d1
+            else:
+                elapsed_days += num_of_days[i]
+        elapsed_days += d2
+    else:
+        elapsed_days = d2 - d1
 else:
-    elapsed_days = d2 - d1
+    if m1 != m2:
+        for i in range(m2, m1):
+            if i == m2:
+                elapsed_days += num_of_days[i] - d2
+            else:
+                elapsed_days += num_of_days[i]
+        elapsed_days += d1
+    else:
+        elapsed_days = d1 - d2
+    elapsed_days = elapsed_days * - 1
 
 d_o_t_w = elapsed_days % 7
 answer = d_o_t_w + 1
