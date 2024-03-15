5d205503889aabc354dbdb32884cf7a5daed488c
SeongUk18 <108208136+SeongUk18@users.noreply.github.com>
2024-03-15 17:37:39 +0900

    Title: 그 요일은, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240315/그 요일은/the-day-of-the-day.py
+++ 240315/그 요일은/the-day-of-the-day.py
@@ -1,24 +1,26 @@
-def calculate_occurrences(m1, d1, m2, d2, target_day):
+def count_weekday_occurrences(m1, d1, m2, d2, weekday):
     days_in_month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    day_names = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+    # 시작일부터 종료일까지 총 일수 계산
+    total_days = sum(days_in_month[m1:m2]) + d2 - d1
+    if m1 == m2:
+        total_days = d2 - d1
     
-    total_days = d2 - d1
-    for month in range(m1, m2):
-        total_days += days_in_month[month]
-    
-    start_day = (sum(days_in_month[:m1]) + d1 - 1) % 7
-    target_day_index = day_names.index(target_day)
-    
-    occurrences = 0
-    for i in range(total_days + 1):
-        if (start_day + i) % 7 == target_day_index:
-            occurrences += 1
+    # 시작 요일부터 총 일수까지 특정 요일이 몇 번 발생하는지 계산
+    start_weekday_index = (days_in_month[m1] - d1) % 7  # m1월 d1일의 요일 인덱스
+    target_index = weekdays.index(weekday)
+
+    # 전체 기간 동안 해당 요일이 발생하는 횟수 계산
+    occurrences = total_days // 7  # 기본적으로 발생하는 주의 횟수
+    if (start_weekday_index + total_days % 7) >= target_index:
+        occurrences += 1
 
     return occurrences
 
-
+# 입력
 m1, d1, m2, d2 = map(int, input().split())
-day = input()
+weekday = input()
 
-
-print(calculate_occurrences(m1, d1, m2, d2, day))
+# 계산 및 결과 출력
+print(count_weekday_occurrences(m1, d1, m2, d2, weekday))
