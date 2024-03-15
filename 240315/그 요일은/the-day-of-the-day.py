6b6baa37278cdd44aef1bab271c8d8e7e99a9edb
SeongUk18 <108208136+SeongUk18@users.noreply.github.com>
2024-03-15 17:30:36 +0900

    Title: 그 요일은, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240315/그 요일은/the-day-of-the-day.py
+++ 240315/그 요일은/the-day-of-the-day.py
@@ -1,30 +1,24 @@
+def calculate_occurrences(m1, d1, m2, d2, target_day):
+    days_in_month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    day_names = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    
+    total_days = d2 - d1
+    for month in range(m1, m2):
+        total_days += days_in_month[month]
+    
+    start_day = (sum(days_in_month[:m1]) + d1 - 1) % 7
+    target_day_index = day_names.index(target_day)
+    
+    occurrences = 0
+    for i in range(total_days + 1):
+        if (start_day + i) % 7 == target_day_index:
+            occurrences += 1
+
+    return occurrences
+
+
 m1, d1, m2, d2 = map(int, input().split())
-day_week = input()
+day = input()
 
-# 1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
-num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-day_of_the_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-index = day_of_the_week.index(day_week)
 
-# 초기 날짜에서 요일의 인덱스 계산
-start_day_index = (d1 - 1) % 7
-
-# 경과 일수 계산
-elapsed_days = 0
-for i in range(m1, m2 + 1):
-    if i == m1:
-        elapsed_days += num_of_days[i] - d1
-    elif i == m2:
-        elapsed_days += d2
-    else:
-        elapsed_days += num_of_days[i]
-
-# 시작 요일부터 계산하여, 해당 요일이 몇 번 등장하는지 계산
-total_weeks, remaining_days = divmod(elapsed_days, 7)
-day_count = total_weeks  # 해당 요일이 최소 등장하는 횟수
-
-# 경과 일수를 기반으로 특정 요일이 추가로 등장하는지 확인
-if remaining_days + start_day_index >= 7:
-    day_count += 1
-
-print(day_count)
+print(calculate_occurrences(m1, d1, m2, d2, day))
