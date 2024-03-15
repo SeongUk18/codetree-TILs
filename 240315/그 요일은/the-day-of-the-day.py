cef9fdf8181e56fbae68a6a6a34b42fbe2cf1387
SeongUk18 <108208136+SeongUk18@users.noreply.github.com>
2024-03-15 16:58:25 +0900

    Title: 그 요일은, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240315/그 요일은/the-day-of-the-day.py
+++ 240315/그 요일은/the-day-of-the-day.py
@@ -2,7 +2,7 @@ m1, d1, m2, d2 = map(int, input().split())
 elapsed_days = 0
 
 #                  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
-num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 if m1 != m2:
     for i in range(m1, m2):
         if i == m1:
