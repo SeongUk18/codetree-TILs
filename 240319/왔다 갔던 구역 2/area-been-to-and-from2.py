80f720e82020986f002f4763cd9aedefd0579548
SeongUk18 <108208136+SeongUk18@users.noreply.github.com>
2024-03-19 17:14:06 +0900

    Title: 왔다 갔던 구역 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240319/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240319/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,20 +1,21 @@
 n = int(input())
 
-way = [0 for _ in range(1000)]
+way = [0 for _ in range(2001)]
 
-start = 0
+start = 1000
+way[start] = 1
 
 for _ in range(n):
     x, direction = input().split()
     x = int(x)
     if direction == "R":
-        for i in range(1, x + 1):
-            start += 1
-            way[start] += 1
+        for i in range(x):
+            way[start + i] += 1
+        start += x
     else:
-        for i in range(1, x + 1):
-            start -= 1
-            way[start] += 1
+        for i in range(x):
+            way[start - i] += 1
+        start -= x
 
 answer = len([x for x in way if x >= 2])
 
