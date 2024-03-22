30f5a18acd0872510a9cae7e3e5d97e302a8029e
SeongUk18 <108208136+SeongUk18@users.noreply.github.com>
2024-03-22 13:24:18 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 237ms, Memory: 89MB, Status: Wrong Answer - CodeTree

--- 240322/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240322/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -1,12 +1,20 @@
 map_list = [[0 for _ in range(2001)] for _ in range(2001)]
 
 x1, y1, x2, y2 = map(int, input().split())
+x1 += 1000
+y1 += 1000
+x2 += 1000
+y2 += 1000
 
 for i in range(y2 - y1 + 1):
     for j in range(x2 - x1 + 1):
         map_list[y1 + i][x1 + j] = 1
 
 x1, y1, x2, y2 = map(int, input().split())
+x1 += 1000
+y1 += 1000
+x2 += 1000
+y2 += 1000
 
 for i in range(y2 - y1):
     for j in range(x2 - x1):
@@ -22,9 +30,9 @@ for i in range(len(map_list)):
             answer_x.append(j)
             answer_y.append(i)
 # print(answer_x, answer_y)
-max_x = max(answer_x)
-min_x = min(answer_x)
-min_y = min(answer_y)
-max_y = max(answer_y)
+max_x = max(answer_x) - 1000
+min_x = min(answer_x) - 1000
+min_y = min(answer_y) - 1000
+max_y = max(answer_y) - 1000
 
 print((max_x - min_x) * (max_y - min_y))
