03c9252532de320c25f8ed95a08b715b6a01f5e4
SeongUk18 <108208136+SeongUk18@users.noreply.github.com>
2024-03-25 21:07:41 +0900

    Title: 연속되는 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240325/연속되는 수 2/continuous-number2.py
+++ 240325/연속되는 수 2/continuous-number2.py
@@ -3,14 +3,18 @@ n = int(input())
 answer = []
 cnt = 0
 before = 123456789
-for i in range(n):
-    number = int(input())
-    if i == 0 or before == number:
-        cnt += 1
-        before = number
-    else:
-        answer.append(cnt)
-        cnt = 1
-        before = number
+if n != 1:
+    for i in range(n):
+        number = int(input())
+        if i == 0 or before == number:
+            cnt += 1
+            before = number
+        else:
+            answer.append(cnt)
+            cnt = 1
+            before = number
+else:
+    answer.append(1)
+
 
 print(max(answer))
