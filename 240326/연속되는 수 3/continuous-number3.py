b5bc08de33e3ffebf787ad94c8ee9a4bbeeb8dfe
SeongUk18 <108208136+SeongUk18@users.noreply.github.com>
2024-03-26 21:29:43 +0900

    Title: 연속되는 수 3, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240326/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+cnt = 1
+answer = 0
+def check(number):
+    if number > 0:
+        flag = True
+    else:
+        flag = False
+    return flag
+
+for i in range(n):
+    number = int(input())
+
+    if i == 0:
+        flag = check(number)
+    
+    if flag == check(number):
+        cnt += 1
+        flag = check(number)
+    else:
+        cnt = 1
+        flag = check(number)
+    
+    answer = max(answer, cnt)
+
+print(answer)
