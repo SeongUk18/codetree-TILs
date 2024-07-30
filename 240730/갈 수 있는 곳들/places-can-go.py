138f8b1f6e52b2f43d428f4acc0e2b2f9092a6f0
Seonguk Han <adulty22@gmail.com>
2024-07-30 20:01:46 +0900

    Title: 갈 수 있는 곳들, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240730/갈 수 있는 곳들/places-can-go.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+n, k = map(int, input().split())
+
+map_list = []
+map_check = [[True for _ in range(n)] for _ in range(n)]
+x_list = [0, 0, -1, 1]
+y_list = [-1, 1, 0, 0]
+q = deque()
+count = 1
+
+for _ in range(n):
+    map_list.append(list(map(int, input().split())))
+
+for _ in range(k):
+    r, c = map(int, input().split())
+    q.append((r, c))
+    map_check[r][c] = False
+
+    while q:
+        cur_x, cur_y = q.popleft()
+
+        for i in range(4):
+            nx = cur_x + x_list[i]
+            ny = cur_y + y_list[i]
+
+            if 0 <= nx < n and 0 <= ny < n and map_check[ny][nx] and map_list[ny][nx] == 0:
+                q.append((nx, ny))
+                map_check[ny][nx] = False
+                count += 1
+
+print(count)
