09103867071e211ccc2bb83ba5e0d28661f78dca
Seonguk Han <adulty22@gmail.com>
2024-09-24 21:29:32 +0900

    Title: 떨어지는 1자 블록, Time: 139ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240924/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240924/떨어지는 1자 블록/falling-horizontal-block.py
@@ -6,18 +6,13 @@ space = []
 for _ in range(n):
     space.append(list(map(int, input().split())))
 
-check = True
+
 # 맵 맨 아래줄부터 m, k 가 들어갈 수 있는지 확인
-for i in range(1, n):
-    if all(space[i][j] == 0 for j in range(k - 1, k - 1 + m)):
+for i in range(n - 1, -1, -1):
+    if all(space[i][j] == 0 for j in range(k - 1, k - 1 + m)) and sum(space[i][k - 1: k + m - 1]) == 0:
         for j in range(k - 1, k - 1 + m):
             space[i][j] = 1
-            check = False
         break
-
-if check:
-    for j in range(k - 1, k - 1 + m):
-        space[0][j] = 1
 
 for i in range(n):
     for j in range(n):
