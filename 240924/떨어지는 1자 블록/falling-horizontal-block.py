289e5515a731bbffad3706288b7cb524152d629c
Seonguk Han <adulty22@gmail.com>
2024-09-24 21:33:49 +0900

    Title: 떨어지는 1자 블록, Time: 131ms, Memory: 25MB, Status: Passed - CodeTree

--- 240924/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240924/떨어지는 1자 블록/falling-horizontal-block.py
@@ -10,6 +10,7 @@ for _ in range(n):
 # 맵 맨 아래줄부터 m, k 가 들어갈 수 있는지 확인
 for i in range(n - 1, -1, -1):
     if all(space[i][j] == 0 for j in range(k - 1, k - 1 + m)) and all(sum(space[z][k - 1: k + m - 1]) == 0 for z in range(i)):
+        # 맨 아랫줄부터 안채워져 있을 수 있으니 확인 필요
         for j in range(k - 1, k - 1 + m):
             space[i][j] = 1
         break
