78c130a2702ac477f663e038bc8e0f9b550a2173
Seonguk Han <adulty22@gmail.com>
2024-10-01 22:32:30 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 168ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241001/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 241001/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -11,7 +11,7 @@ for _ in range(n):
 
 for i in range(m):
     r, c = map(int, input().split())
-    bead[i].append([r - 1, c - 1]) # 좌표값이 1부터 시작해서 -1 추가
+    bead[i].append([r - 1, c - 1])  # 좌표값이 1부터 시작해서 -1 추가
 
 # 상하좌우 확인용
 x_list = [0, 0, -1, 1]
@@ -24,23 +24,24 @@ for _ in range(t):
         if not locate:
             continue
         x, y = locate[-1]
-        max_num = -1
-        next_locate = []
+        max_num = 0
+        next_locate = [x, y]
         for k in range(4):
             cur_x = x + x_list[k]
             cur_y = y + y_list[k]
             if 0 <= cur_x < n and 0 <= cur_y < n:
-                if max_num < map_list[cur_y][cur_x]:
-                    max_num = map_list[cur_y][cur_x]
+                if max_num < map_list[cur_x][cur_y]:
+                    max_num = map_list[cur_x][cur_y]
                     next_locate = [cur_x, cur_y]
 
         if next_locate:
             next_positions[tuple(next_locate)].append(key)
-        
+
     bead.clear()
     for position, keys in next_positions.items():
         if len(keys) == 1:
             bead[keys[0]].append(list(position))
+    next_positions.clear()
 
-                
+
 print(len(bead))
