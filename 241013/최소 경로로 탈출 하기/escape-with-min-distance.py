1b8a9c0b9b037ac26f04de4807eecb5573325e7d
Seonguk Han <adulty22@gmail.com>
2024-10-13 14:14:05 +0900

    Title: 최소 경로로 탈출 하기, Time: 137ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241013/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+n, m = map(int, input().split())
+
+map_check = [[False for _ in range(m)] for _ in range(n)]
+
+map_list = []
+
+for _ in range(n):
+    map_list.append(list(map(int, input().split())))
+
+x_list = [0, 0, -1, 1]
+y_list = [-1, 1, 0, 0]
+
+def bfs():
+    q = deque()
+    q.append((0, 0, 0))
+    map_check[0][0] = True
+
+    while q:
+        x, y, dist = q.popleft()
+        
+
+        for i in range(4):
+            cur_x = x + x_list[i]
+            cur_y = y + y_list[i]
+
+            if 0 <= cur_x < m and 0 <= cur_y < n and map_check[cur_x][cur_y] == False and map_list[cur_x][cur_y] == 1:
+                q.append((cur_x, cur_y, dist + 1)) 
+                map_check[cur_x][cur_y] = True
+
+    return dist
+
+answer = bfs()
+
+if answer == 0:
+    print(0)
+else:
+    print(answer)
