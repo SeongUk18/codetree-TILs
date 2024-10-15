f4a19ea63a31b981fd0a06c58e9a602b5093d6dc
Seonguk Han <adulty22@gmail.com>
2024-10-15 14:38:53 +0900

    Title: K번 최댓값으로 이동하기, Time: 158ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241015/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+# 입력 받기
+n, k = map(int, input().split())
+
+map_list = []
+
+for _ in range(n):
+    map_list.append(list(map(int, input().split())))
+
+r, c = map(int, input().split())
+r -= 1
+c -= 1  # 값 보정
+
+x_list = [0, 0, -1, 1]
+y_list = [-1, 1, 0, 0]
+
+q = deque()
+q.append((r, c))
+
+for _ in range(k):
+    x, y = q.popleft()
+    next_idx = (x, y)
+    target = float('-inf')
+    for i in range(n):
+        for j in range(n):
+            if map_list[i][j] < map_list[x][y] and target < map_list[i][j]:
+                target = map_list[i][j]
+                next_idx = (i, j)
+
+    if next_idx[0] == x and next_idx[1] == y:
+        break
+    else:
+        q.append((next_idx[0], next_idx[1]))
+
+for i, j in q:
+    print(i, j)
