e2d4dfa631a3c155d84dbf150e6c4983ef881cdc
Seonguk Han <adulty22@gmail.com>
2024-10-21 09:07:20 +0900

    Title: 부분 수열의 합이 m, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

--- 241021/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.py
+++ 241021/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.py
@@ -6,12 +6,11 @@ dp = [float('inf') for _ in range(m + 1)]
 
 dp[0] = 0
 
-for i in range(1, n):
-    for j in range(m - 1, -1, -1):
-        if num_list[i] <= j:
-            dp[j] = min(dp[j], dp[j - num_list[i]] + 1)
+for i in range(n):
+    for j in range(m, num_list[i] - 1, -1):
+        dp[j] = min(dp[j], dp[j - num_list[i]] + 1)
 
-if dp[m - 1] == float('inf'):
+if dp[m] == float('inf'):
     print(-1)
 else:
-    print(dp[m - 1])
+    print(dp[m])
