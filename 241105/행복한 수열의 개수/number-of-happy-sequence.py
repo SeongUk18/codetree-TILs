d0accfe7c2fc32dc5188053660e2fefb0b415397
Seonguk Han <adulty22@gmail.com>
2024-11-05 12:04:16 +0900

    Title: 행복한 수열의 개수, Time: 55ms, Memory: 16MB, Status: Passed - CodeTree

--- 241105/행복한 수열의 개수/number-of-happy-sequence.py
+++ 241105/행복한 수열의 개수/number-of-happy-sequence.py
@@ -10,36 +10,36 @@ answer = 0
 for i in range(n):
     prev_num = num_list[i][0]
     count = 1
+    found = False  # 중복 카운트 방지용 변수 추가
     for j in range(1, n):
-        if prev_num == num_list[i][j]:
+        if num_list[i][j] == prev_num:
             count += 1
         else:
             if count >= m:
                 answer += 1
+                found = True
                 break
             prev_num = num_list[i][j]
             count = 1
-
-    # 마지막 구간 검사
-    if count >= m:
+    if count >= m and not found:  # 마지막 구간 검사
         answer += 1
 
 # 세로 확인
 for i in range(n):
     prev_num = num_list[0][i]
     count = 1
+    found = False  # 중복 카운트 방지용 변수 추가
     for j in range(1, n):
-        if prev_num == num_list[j][i]:
+        if num_list[j][i] == prev_num:
             count += 1
         else:
             if count >= m:
                 answer += 1
+                found = True
                 break
             prev_num = num_list[j][i]
             count = 1
-
-    # 마지막 구간 검사
-    if count >= m:
+    if count >= m and not found:  # 마지막 구간 검사
         answer += 1
 
 print(answer)
