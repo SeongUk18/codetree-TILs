f2ed1327ee344874e8ec6af6571d220887c6e373
Seonguk Han <adulty22@gmail.com>
2024-11-19 16:19:07 +0900

    Title: 정수 명령 처리 6, Time: 237ms, Memory: 23MB, Status: Passed - CodeTree

--- 241119/정수 명령 처리 6/process-numeric-commands-6.py
+++ 241119/정수 명령 처리 6/process-numeric-commands-6.py
@@ -1,22 +1,24 @@
 import heapq
 
-n = int(input())
+n = int(input())  
 
-heap = []
+heap = []  
 
 for _ in range(n):
-    command = input()
-    if command[:3] == "pop":
-        print(heapq.heappop(heap))
-    elif command[0] == "s":
-        print(len(heap))
-    elif command[0] == "e":
-        if len(heap) == 0:
-            print(1)
-        else:
-            print(0)
-    elif command[0] == "p":
-        comand, num = command.split()
-        heapq.heappush(heap, int(num))
-    elif command[0] == "t":
-        print(heap[-1])
+    command = input().strip()
+    if command == "pop":
+        print(-heapq.heappop(heap))  # 음수로 저장된 값을 다시 양수로 변환하여 출력
+
+    elif command == "size":
+        print(len(heap)) 
+
+    elif command == "empty":
+        print(1 if not heap else 0)  # 힙이 비어 있으면 1, 아니면 0 출력
+
+    elif command.startswith("push"):
+        _, num = command.split()
+        heapq.heappush(heap, -int(num))  # 음수로 변환하여 최대 힙 구현
+        
+    elif command == "top":
+        print(-heap[0])  # 음수로 저장된 최댓값을 다시 양수로 변환하여 출력
+
