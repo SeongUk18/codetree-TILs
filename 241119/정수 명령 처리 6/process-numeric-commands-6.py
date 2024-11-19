be05f832e83399bf9baa0db5a370c01f618c672f
Seonguk Han <adulty22@gmail.com>
2024-11-19 16:11:35 +0900

    Title: 정수 명령 처리 6, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241119/정수 명령 처리 6/process-numeric-commands-6.py
@@ -0,0 +1,20 @@
+import heapq
+
+n = int(input())
+
+heap = []
+
+for _ in range(n):
+    command = input()
+    if command[:3] == "pop":
+        print(heap.pop())
+    elif command[0] == "s":
+        print(len(heap))
+    elif command[0] == "e":
+        if len(heap) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == "p":
+        comand, num = command.split()
+        heapq.heappush(heap, int(num))
