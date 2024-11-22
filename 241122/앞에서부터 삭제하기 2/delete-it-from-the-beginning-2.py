a87c5efdb7f800a6697e9c610c82420f098a450a
Seonguk Han <adulty22@gmail.com>
2024-11-22 13:52:18 +0900

    Title: 앞에서부터 삭제하기 2, Time: 5103ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 241122/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 241122/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -6,12 +6,20 @@ num_list = list(map(int, input().split()))
 
 max_avg = 0
 
+# 누적 합 계산
+prefix_sum = [0] * (n + 1)
+for i in range(n):
+    prefix_sum[i + 1] = prefix_sum[i] + num_list[i]
+
+
 for i in range(1, n - 1):
     new_num_list = num_list[i:]
 
     heapq.heapify(new_num_list)
-    heapq.heappop(new_num_list)
+    smallest = heapq.heappop(new_num_list)
 
-    max_avg = max(max_avg, sum(new_num_list) / len(new_num_list))
+    total_sum = prefix_sum[n] - prefix_sum[i] - smallest
+
+    max_avg = max(max_avg, total_sum / len(new_num_list))
 
 print(f"{max_avg:0.2f}")
