fc4eab73befce261ff9c63344d32d8cff3efe4df
Seonguk Han <adulty22@gmail.com>
2025-01-21 10:56:42 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 5091ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 250121/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 250121/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -3,19 +3,24 @@ points = [tuple(map(int, input().split())) for _ in range(n)]
 x = [p[0] for p in points]
 y = [p[1] for p in points]
 
-answer = []
+visited = [False for _ in range(n)]
+min_dist = float('inf')
 
 def backtracking(cur_x, cur_y, n, road, dist):
-    if road == n - 3:
-        answer.append(dist + abs(cur_x - x[-1]) + abs(cur_y - y[-1]))
-        return
-    elif road > n - 3:
-        return
+    global min_dist
 
-    for i, j in points[1: -1]:
-        backtracking(i, j, n, road + 1, dist + abs(cur_x - i) + abs(cur_y - j))
-        
+    if road == n - 3:
+        dist = dist + abs(cur_x - x[-1]) + abs(cur_y - y[-1])
+        min_dist = min(dist, min_dist)
+        return 
+
+    for idx in range(1, n - 1):
+        visited[idx] = True
+        backtracking(x[idx], y[idx], n, road + 1, dist + abs(cur_x - x[idx]) + abs(cur_y - y[idx]))
+        visited[idx] = False
+
+visited[0] = True
 backtracking(x[0], y[0], n, 0, 0)
 
 
-print(min(answer))
+print(min_dist)
