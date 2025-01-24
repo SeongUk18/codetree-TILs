154d6a67307e11c3e84ef804b287a60ba41da683
Seonguk Han <adulty22@gmail.com>
2025-01-24 10:23:49 +0900

    Title: 오목, Time: 78ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250124/오목/O-mok.py
+++ 250124/오목/O-mok.py
@@ -35,3 +35,13 @@ for i in range(19):
                 print(color)
                 print(i + k - 1, j + k - 1)
                 break
+
+        cnt = 0
+        for k in range(5):
+            if if i >=4 and color == board[i - k][j + k]:
+                cnt += 1
+
+            if cnt == 5:
+                print(color)
+                print(i + 2, j + k - 1)
+                break
