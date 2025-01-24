20a70082a113dccaa14d95da22c170adee916e77
Seonguk Han <adulty22@gmail.com>
2025-01-24 10:25:59 +0900

    Title: 오목, Time: 71ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250124/오목/O-mok.py
+++ 250124/오목/O-mok.py
@@ -43,5 +43,5 @@ for i in range(19):
 
             if cnt == 5:
                 print(color)
-                print(i - 2, j + k - 1)
+                print(i - 1, j + k - 1)
                 break
