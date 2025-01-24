12f6dd5b5a9ea32dfb0fbe73fb5d22369ac36541
Seonguk Han <adulty22@gmail.com>
2025-01-24 10:20:36 +0900

    Title: 오목, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250124/오목/O-mok.py
@@ -0,0 +1,27 @@
+board = [list(map(int, input().split())) for _ in range(19)]
+
+for i in range(19):
+    for j in range(19):
+        if board[i][j] == 0:
+            continue
+        
+        color = board[i][j]
+        cnt = 0
+        for k in range(5):
+            if color == board[i][j + k]:
+                cnt += 1
+
+            if cnt == 5:
+                print(color)
+                print(i + 1, j + k - 1)
+                break
+
+        cnt = 0
+        for k in range(5):
+            if color == board[i + k][j]:
+                cnt += 1
+
+            if cnt == 5:
+                print(color)
+                print(i + k - 1, j + 1)
+                break
