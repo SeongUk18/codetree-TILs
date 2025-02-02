13c234648091b440fd535d4c5c1a56e278fa274c
Seonguk Han <adulty22@gmail.com>
2025-02-02 16:57:35 +0900

    Title: G or H 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250202/G or H 2/G-or-H-2.py
+++ 250202/G or H 2/G-or-H-2.py
@@ -1,24 +1,26 @@
 N = int(input())
-people = []
+people = [tuple(input().split()) for _ in range(N)]
+people = sorted((int(pos), ch) for pos, ch in people)
 
-for _ in range(N):
-    pos, ch = input().split()
-    people.append((int(pos), ch))
-
-people.sort()
+positions = [pos for pos, ch in people] 
+types = [ch for pos, ch in people] 
 
 max_size = 0
 
 for i in range(N):
-    g_count = 0
-    h_count = 0
+    g_count = h_count = 0
     for j in range(i, N):
-        if people[j][1] == 'G':
+        if types[j] == 'G':
             g_count += 1
         else:
             h_count += 1
-        
         if g_count == h_count:
-            max_size = max(max_size, people[j][0] - people[i][0])
+            max_size = max(max_size, positions[j] - positions[i])
+
+if types.count('H') == 0:
+    max_size = max(max_size, positions[-1] - positions[0])
+
+if types.count('G') == 0:
+    max_size = max(max_size, positions[-1] - positions[0])
 
 print(max_size)
