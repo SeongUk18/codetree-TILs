583628304d336c1346a6602b5a9e267137187caf
Seonguk Han <adulty22@gmail.com>
2025-02-02 17:07:52 +0900

    Title: G or H 2, Time: 51ms, Memory: 16MB, Status: Passed - CodeTree

--- 250202/G or H 2/G-or-H-2.py
+++ 250202/G or H 2/G-or-H-2.py
@@ -2,7 +2,7 @@ N = int(input())
 people = [tuple(input().split()) for _ in range(N)]
 people = sorted((int(pos), ch) for pos, ch in people)
 
-positions = [pos for pos, ch in people] 
+positions = [pos for pos, ch in people]  
 types = [ch for pos, ch in people] 
 
 max_size = 0
@@ -14,13 +14,28 @@ for i in range(N):
             g_count += 1
         else:
             h_count += 1
-        if g_count == h_count:
+        if g_count == h_count:  
             max_size = max(max_size, positions[j] - positions[i])
 
-if types.count('H') == 0:
-    max_size = max(max_size, positions[-1] - positions[0])
+max_g_run = 0
+start = None
+for i in range(N):
+    if types[i] == 'G':
+        if start is None:
+            start = positions[i]
+        max_g_run = max(max_g_run, positions[i] - start)
+    else:
+        start = None
 
-if types.count('G') == 0:
-    max_size = max(max_size, positions[-1] - positions[0])
+max_h_run = 0
+start = None
+for i in range(N):
+    if types[i] == 'H':
+        if start is None:
+            start = positions[i]
+        max_h_run = max(max_h_run, positions[i] - start)
+    else:
+        start = None
 
+max_size = max(max_size, max_g_run, max_h_run)
 print(max_size)
