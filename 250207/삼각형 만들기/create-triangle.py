18ddfc675910ce38238689906c25a37993a98166
Seonguk Han <adulty22@gmail.com>
2025-02-07 14:52:56 +0900

    Title: 삼각형 만들기, Time: 100ms, Memory: 19MB, Status: Passed - CodeTree

--- 250207/삼각형 만들기/create-triangle.py
+++ 250207/삼각형 만들기/create-triangle.py
@@ -1,32 +1,26 @@
+
 n = int(input())
-points = [tuple(map(int, input().split())) for _ in range(n)]
-x = [p[0] for p in points]
-y = [p[1] for p in points]
+points = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
 
-area = 0
+
+def area(x1, y1, x2, y2, x3, y3):
+    return abs((x1 * y2 + x2 * y3 + x3 * y1) -
+               (x2 * y1 + x3 * y2 + x1 * y3))
+
+
+max_area = 0
 for i in range(n):
-    for j in range(n):
-        if i == j:
-            continue
-        
-        if x[i] == x[j]:
-            for k in range(n):
-                if k == i or k == j:
-                    continue
-                else:
-                    if y[j] == y[k]:
-                        area = abs(x[k] - x[i]) * abs(y[i] - y[k])
-                    elif y[i] == y[k]:
-                        area = abs(x[k] - x[i]) * abs(y[j] - y[k])
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            x1, y1 = points[i]
+            x2, y2 = points[j]
+            x3, y3 = points[k]
+            if (x1 == x2 or x1 == x3 or x2 == x3) and \
+                (y1 == y2 or y1 == y3 or y2 == y3):
+                max_area = max(max_area, 
+                               area(x1, y1, x2, y2, x3, y3))
 
-        elif y[i] == y[j]:
-            for k in range(n):
-                if k == i or k == j:
-                    continue
-                else:
-                    if x[j] == x[k]:
-                        area = abs(y[k] - y[i]) * abs(x[i] - x[k])
-                    elif x[i] == x[k]:
-                        area = abs(y[k] - y[i]) * abs(x[j] - x[k])
-
-print(area)
+print(max_area)
