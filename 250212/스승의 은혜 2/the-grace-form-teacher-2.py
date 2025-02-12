b202dfb8999824e1d540ef3a9d5223c7b7c12e4e
Seonguk Han <adulty22@gmail.com>
2025-02-12 22:08:56 +0900

    Title: 스승의 은혜 2, Time: 52ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250212/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,22 @@
+N, B = map(int, input().split())
+P = [int(input()) for _ in range(N)]
+
+P.sort()
+
+max_num = float('-inf')
+for i in range(N):
+    cost = 0
+    num = 0
+    for j in range(N):
+        if i == j:
+            num += 1
+            cost += P[i] / 2
+        else:
+            num += 1
+            cost += P[i]
+
+        if cost >= B:
+            break
+    max_num = max(max_num, num - 1)
+
+print(max_num)
