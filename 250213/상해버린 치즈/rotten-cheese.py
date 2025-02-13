3cf739ed75aa66cd7dbe81325a47f088c8c1d37b
Seonguk Han <adulty22@gmail.com>
2025-02-13 15:05:14 +0900

    Title: 상해버린 치즈, Time: 50ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250213/상해버린 치즈/rotten-cheese.py
+++ 250213/상해버린 치즈/rotten-cheese.py
@@ -10,22 +10,27 @@ for _ in range(D):
     t.append(time)
 
 sick_dict = defaultdict(int)
+sick_p = []
 for _ in range(S):
     person, time = map(int, input().split())
     sick_dict[person] = time
+    sick_p.append(person)
 
 milk_dict = defaultdict(list)
-
+eat_mike = defaultdict(list)
 for i in range(D):
     milk_dict[m[i]].append((p[i], t[i]))
+    eat_mike[m[i]].append(p[i])
 
 # print(milk_dict)
 # print(sick_dict)
 
 max_person = float('-inf')
 
+
 for key, val in milk_dict.items():
-    if all(person in sick_dict or time > sick_dict[person] for person, time in val):
-        max_person = max(max_person, len(val))
+    if all(s_p in eat_mike[key] for s_p in sick_p):
+        if all(person in sick_dict or time > sick_dict[person] for person, time in val):
+            max_person = max(max_person, len(val))
 
 print(max_person)
