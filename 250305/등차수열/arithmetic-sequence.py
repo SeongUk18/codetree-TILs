8b2774be484709fe88b4272c703ff68d750bef95
Seonguk Han <adulty22@gmail.com>
2025-03-05 17:58:37 +0900

    Title: 등차수열, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250305/등차수열/arithmetic-sequence.py
+++ 250305/등차수열/arithmetic-sequence.py
@@ -4,12 +4,12 @@ a = list(map(int, input().split()))
 answer = 0
 
 for i in range(n):
-    for k in range(min(a), max(a)):
-        diff = k - a[i]
-
-        if (diff + k) > a[i] and (diff + k) in a:
-            # print(a[i], diff, diff + k, k)
+    for j in range(i + 1, n):
+        # K를 계산
+        k = (a[i] + a[j]) // 2
+        
+        # 등차수열 조건 확인
+        if (a[j] - k) == (k - a[i]):
             answer += 1
-    
 
 print(answer)
