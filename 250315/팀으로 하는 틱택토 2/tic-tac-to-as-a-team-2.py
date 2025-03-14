38d7a87acf35dc3ffe4048e0a1e775999435c591
Seonguk Han <adulty22@gmail.com>
2025-03-15 03:37:18 +0900

    Title: 팀으로 하는 틱택토 2, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250315/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 250315/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -1,7 +1,7 @@
 inp = [input() for _ in range(3)]
 
 N = 3
-answer = 0
+answer = set()
 
 for i in range(N):
     team_set = set()
@@ -9,7 +9,8 @@ for i in range(N):
         team_set.add(inp[i][j])
     # print(team_set)
     if len(team_set) == 2:
-        answer += 1
+        # print(team_set)
+        answer.add(tuple(team_set))
 
 for i in range(N):
     team_set = set()
@@ -17,20 +18,24 @@ for i in range(N):
         team_set.add(inp[j][i])
     # print(team_set)    
     if len(team_set) == 2:
-        answer += 1
+        # print(team_set)
+        answer.add(tuple(team_set))
 
 team_set = set()
 for i in range(N):
     team_set.add(inp[i][i])
 # print(team_set)
 if len(team_set) == 2:
-    answer += 1
+    # print(team_set)
+    answer.add(tuple(team_set))
 
 team_set = set()
 for i in range(N):
     team_set.add(inp[2 - i][i])
 # print(team_set)
 if len(team_set) == 2:
-    answer += 1
+    # print(team_set)
+    answer.add(tuple(team_set))
 
-print(answer)
+
+print(len(answer))
