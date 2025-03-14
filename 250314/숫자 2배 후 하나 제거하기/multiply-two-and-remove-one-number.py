36e03bfb42378c41568aad67c9fe173a0b3b025f
Seonguk Han <adulty22@gmail.com>
2025-03-14 16:27:08 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: 71ms, Memory: 18MB, Status: Wrong Answer - CodeTree

--- 250314/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.py
+++ 250314/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.py
@@ -6,21 +6,24 @@ arr = list(map(int, input().split()))
 min_dist = float('inf')
 for i in range(n):
     new_arr = copy.deepcopy(arr)
-    # print(new_arr)
     new_arr[i] = arr[i] * 2
-
+    # print(new_arr)
     for j in range(n):
-        dist = 0
+        if i == j:
+            continue
         prev = False
         prev_num = 0
+        # print(new_arr)
         for k in range(n):
             if j == k:
                 continue
             if prev == False:
                 prev_num = new_arr[k]
                 prev = True
+                dist = 0
             else:
                 dist += abs(prev_num - new_arr[k])
+                # print(j)
                 # print(prev_num, new_arr[k])
                 # print(dist)
         min_dist = min(min_dist, dist)
