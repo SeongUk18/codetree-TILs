956b53502c5346175640c7e9814052d34612b859
Seonguk Han <adulty22@gmail.com>
2025-03-15 04:24:32 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 50ms, Memory: 16MB, Status: Passed - CodeTree

--- 250315/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 250315/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -3,7 +3,9 @@ str = input()
 
 def find(N, str):
     for i in range(1, N):
-        if all(str[j:j + i] not in str[j + i:] for j in range(N - i)):
+        if any(str[j:j + i] in str[j + 1:] for j in range(N)):
+            continue
+        else:
             return i
                    
 
