38b299ff7b1f91b50dd9ee7cf8aa5c9e8ae85ac7
Seonguk Han <adulty22@gmail.com>
2025-03-15 04:57:49 +0900

    Title: 원소 값들의 최대 합, Time: 66ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250315/원소 값들의 최대 합/maximum-sum-of-element-values.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+max_sum = float('-inf')
+for i in range(1, n + 1):
+    n_dict = {k + 1 : arr[k] for k in range(n)}
+    start = i
+    point = 0
+    for _ in range(m):
+        point += n_dict[start]
+        next = n_dict[start]
+        # print(start, n_dict[start])
+        # print(next, n_dict[next])
+        n_dict[start], n_dict[next] = n_dict[next], n_dict[start]
+        # start = n_dict[start]
+        # print(start)
+        # print(point)
+    # print(point)
+    # print("---")
+    max_sum = max(max_sum, point)
+
+print(max_sum)
