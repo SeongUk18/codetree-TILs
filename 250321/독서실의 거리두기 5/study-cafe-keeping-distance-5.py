e8290d4ee3505cbcf4d740840bb444e453852d03
Seonguk Han <adulty22@gmail.com>
2025-03-21 15:58:39 +0900

    Title: 독서실의 거리두기 5, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250321/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 250321/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -6,27 +6,19 @@ seat = list(seat)
 
 for i in range(N):
     if seat[i] == "0":
-        new_seat = seat[:]
-        front_seat = seat[:i]
-        back_seat = seat[i + 1:]
-        f_dist = 1
-        b_dist = 1
-
-        for j in range(len(front_seat) - 1, -1, -1):
-            if front_seat[j] == "1":
-                break
-            else:
-                f_dist += 1
-        
-        for j in range(len(back_seat)):
-            if back_seat[j] == "1":
-                break
-            else:
-                b_dist += 1
-
-        if i == N - 1:
-            b_dist = 20
-        max_dist = max(max_dist, min(f_dist, b_dist))
-
-
+        new_seat = seat.copy()
+        new_seat[i] = "1"
+        seat_list = []
+        # print(new_seat)
+        min_dist = float('inf')
+        for j in range(N):
+            if new_seat[j] == "1":
+                seat_list.append(j)
+        start = seat_list[0]
+        # print(seat_list)
+        for s in seat_list[1:]:
+            diff = s - start
+            start = s
+            min_dist = min(min_dist, diff)
+        max_dist = max(max_dist, min_dist)
 print(max_dist)
