e58818cbe6bfb5946917efdac2f7f1f6446fe024
Seonguk Han <adulty22@gmail.com>
2025-03-21 15:40:43 +0900

    Title: 독서실의 거리두기 5, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250321/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 250321/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -3,32 +3,30 @@ seat = input()
 
 max_dist = float('-inf')
 seat = list(seat)
-# print(seat)
+
 for i in range(N):
     if seat[i] == "0":
         new_seat = seat[:]
-        new_seat[i] = "1"
-        min_dist = float('inf')
-        dist = 1
-        dist_list = []
-        # print(new_seat)
-        for j in range(N):
-            if j == N - 1:
-                if new_seat[j] == "0":
-                    dist += 1
-                dist_list.append(dist)
-            if j == 0:
-                if new_seat[j] == "1":
-                    continue
+        front_seat = seat[:i]
+        back_seat = seat[i + 1:]
+        f_dist = 1
+        b_dist = 1
 
-            if new_seat[j] == "1":
-                # print(j, dist)
-                dist_list.append(dist)
-                dist = 1
+        for j in range(len(front_seat) - 1, -1, -1):
+            if front_seat[j] == "1":
+                break
             else:
-                dist += 1
-        # print(dist_list)
-        max_dist = max(min(dist_list), max_dist)
+                f_dist += 1
+        
+        for j in range(len(back_seat)):
+            if back_seat[j] == "1":
+                break
+            else:
+                b_dist += 1
+
+        if i == N - 1:
+            b_dist = 20
+        max_dist = max(max_dist, min(f_dist, b_dist))
 
 
 print(max_dist)
