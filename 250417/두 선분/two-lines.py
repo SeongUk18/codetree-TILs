c274eea0020a62fef89d9a28a6eda461e15ed926
Seonguk Han <adulty22@gmail.com>
2025-04-17 21:23:15 +0900

    Title: 두 선분, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250417/두 선분/two-lines.py
@@ -0,0 +1,14 @@
+x1, x2, x3, x4 = map(int, input().split())
+
+x_list = list(range(x1, x2 + 1))
+y_list = list(range(x3, x4 + 1))
+
+def solve():
+    for x in x_list:
+        if x in x_list:
+            print("intersecting")
+            return
+
+    print("noninterssecting")
+
+solve()
