d2149f1780b043e3be0ba59bd86a157c800e6035
Seonguk Han <adulty22@gmail.com>
2025-12-18 10:53:06 +0900

    Title: 코딩톡, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251218/코딩톡/coding-talk.py
@@ -0,0 +1,19 @@
+n, m, p = map(int, input().split())
+messages = [tuple(input().split()) for _ in range(m)]
+c = [msg[0] for msg in messages]
+u = [int(msg[1]) for msg in messages]
+
+people = [chr(i) for i in range(65, 65 + n)]
+# print(people)
+
+# Please write your code here.
+read_people = set()
+for i in range(p - 1, m):
+    read_people.add(c[i])
+
+for i in read_people:
+    people.remove(i)
+
+# print(people)
+for p in people:
+    print(p, end=" ")
