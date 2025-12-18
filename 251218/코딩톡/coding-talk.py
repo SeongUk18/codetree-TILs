61963810a1855027ef1c866fe148abecd58c28cc
Seonguk Han <adulty22@gmail.com>
2025-12-18 11:12:56 +0900

    Title: 코딩톡, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251218/코딩톡/coding-talk.py
+++ 251218/코딩톡/coding-talk.py
@@ -4,22 +4,18 @@ c = [msg[0] for msg in messages]
 u = [int(msg[1]) for msg in messages]
 
 people = [chr(i) for i in range(65, 65 + n)]
-# print(people)
 
-# Please write your code here.
+# p번째 메시지를 확실히 읽은 사람들
 read_people = set()
-for i in range(p - 1, m):
+
+# 1. p번째 메시지를 보낸 사람
+read_people.add(c[p - 1])
+
+# 2. p번째 이후 메시지를 보낸 사람
+for i in range(p, m):
     read_people.add(c[i])
 
-if p >= 2 and u[p - 1] == u[p - 2]:
-    read_people.add(c[p - 2])
-
-for i in read_people:
-    people.remove(i)
-
-# print(people)
-if u[-1] == 0:
-    print("")
-else:
-    for p in people:
-        print(p, end=" ")
+# 읽지 않았을 가능성이 있는 사람들 출력
+for person in people:
+    if person not in read_people:
+        print(person, end=" ")
