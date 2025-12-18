3f8b716c9fbaf76daca2762d4176d3f921ab4a73
Seonguk Han <adulty22@gmail.com>
2025-12-18 12:04:52 +0900

    Title: 연속된 숫자 만들기 2, Time: 53ms, Memory: 16MB, Status: Passed - Codetree

--- 251218/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 251218/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -7,8 +7,6 @@ second_dist = abs(pos[1] - pos[2])
 
 if first_dist == second_dist == 1:
     print(0)
-elif first_dist == 1 or second_dist == 1:
-    print(1)
 elif first_dist == 2 or second_dist == 2:
     print(1)
 else:
