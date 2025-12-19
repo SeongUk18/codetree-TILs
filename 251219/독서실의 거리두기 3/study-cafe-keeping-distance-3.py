36108741e4c29516eaf1e6ac12087d0898dfa3fa
Seonguk Han <adulty22@gmail.com>
2025-12-19 12:08:44 +0900

    Title: 독서실의 거리두기 3, Time: 60ms, Memory: 17MB, Status: Passed - Codetree

--- 251219/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
+++ 251219/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -7,13 +7,16 @@ max_dist = float('-inf')
 for i in range(N):
     if seats[i] == "0":
         seats[i] = "1"
-        dist = 0
-        for j in range(N):
-            if seats[j] == "1":
+        dist = 1
+        dists = []
+        for j in range(1, N):
+            if seats[j] == "0":
                 dist += 1
             else:
-                max_dist = max(dist, max_dist)
-                dist = 0
+                dists.append(dist)
+                dist = 1
+        # print(dists)
         seats[i] = "0"
+        max_dist = max(max_dist, min(dists))
 
 print(max_dist)
