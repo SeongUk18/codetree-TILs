62e4afc3637b34bdd4c91aa0e520ad84b0b87150
Seonguk Han <adulty22@gmail.com>
2025-12-22 12:23:17 +0900

    Title: 독서실의 거리두기 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251222/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 251222/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -3,21 +3,27 @@ seats = list(input())
 
 # Please write your code here.
 max_dist = float('-inf')
+flag = False
 
 for i in range(N):
-    if seats[i] == "0":
+    if flag == False and seats[i] == "1":
+        flag = True
+        start = i
+
+    if seats[i] == "0" and flag:
         seats[i] = "1"
         dists = []
         dist = 1
-        for j in range(1, N):
-            
+        for j in range(start + 1, N):
             if seats[j] == "0":
                 dist += 1
             else:
                 dists.append(dist)
                 dist = 1
+
         # print(dists)
         max_dist = max(max_dist, min(dists))
         seats[i] = "0"
 
+# print(start)
 print(max_dist)
