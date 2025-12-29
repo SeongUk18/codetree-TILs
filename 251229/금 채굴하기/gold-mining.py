78dc86542d75bf5c7216acfa3b28dc9529e6e12d
Seonguk Han <adulty22@gmail.com>
2025-12-29 15:20:08 +0900

    Title: 금 채굴하기, Time: 90ms, Memory: 19MB, Status: Wrong Answer - Codetree

--- 251229/금 채굴하기/gold-mining.py
+++ 251229/금 채굴하기/gold-mining.py
@@ -10,27 +10,24 @@ y_list = [-1, 1, 0, 0]
 q = deque()
 answer = 0
 
-def bfs(n, max_dist):
-    map_list = [[False for _ in range(n)] for _ in range(n)]
-
+def bfs(n, max_dist, gold):
+    total_gold = gold
     while q:
-        x, y, dist, count = q.popleft()
-
-        if dist > max_dist:
-            break
+        x, y, dist = q.popleft()
+        # print(x, y, dist, count)
 
         for i in range(4):
             cur_x = x + x_list[i]
             cur_y = y + y_list[i]
 
-            if 0 <= cur_x < n and 0 <= cur_y < n and map_list[cur_x][cur_y] == False:
+            if 0 <= cur_x < n and 0 <= cur_y < n and map_list[cur_x][cur_y] == False and dist + 1 <= max_dist:
                 if grid[cur_x][cur_y] == 1:
-                    count += 1
+                    total_gold += 1
 
-                q.append((cur_x, cur_y, dist + 1, count))
+                q.append((cur_x, cur_y, dist + 1))
                 map_list[cur_x][cur_y] = True
 
-    return count
+    return total_gold
 
 def price(k):
     return k * k + (k + 1) * (k + 1)
@@ -38,18 +35,19 @@ def price(k):
 
 for i in range(n):
     for j in range(n):
-        for k in range(m):
+        for k in range(n):
+            map_list = [[False for _ in range(n)] for _ in range(n)]
             if grid[i][j] == 1:
-                q.append((i, j, 0, 1))
+                q.append((i, j, 0))
                 map_list[i][j] = True
-                count = bfs(n, k + 1)
+                count = bfs(n, k, 1)
             else:
-                q.append((i, j, 0, 0))
+                q.append((i, j, 0))
                 map_list[i][j] = True
-                count = bfs(n, k + 1)
+                count = bfs(n, k, 0)
 
-        if price(k) <= count * m:
-            answer = max(answer, count)
+            if price(k) <= count * m:
+                answer = max(answer, count)
 
 
 print(answer)
