00099cd41cf47a332553620d2a9d7519d9578ffc
Seonguk Han <adulty22@gmail.com>
2026-03-26 14:22:50 +0900

    Title: 계단 오르기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260326/계단 오르기/climbing-stairs.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+dp = [0] * (N + 1)
+
+dp[0] = 1
+
+if N < 2:
+    print(0)
+    exit()
+for i in range(2, N + 1):
+    if i >= 2:
+        dp[i] += dp[i - 2]
+    if i >= 3:
+        dp[i] += dp[i - 3]
+
+print(dp[N])
