12d6cfd360c895d6b86a0a530d332af4cd425cd1
Seonguk Han <adulty22@gmail.com>
2026-03-30 15:16:36 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260330/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
+++ 260330/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -14,6 +14,6 @@ dp = [1] * n
 for i in range(n):
     for j in range(i + 1, n):
         if lines[j][0] > lines[i][1]:
-            dp[i] = max(dp[i] + 1, dp[j])
+            dp[j] = max(dp[j], dp[i] + 1)
 
 print(max(dp))
