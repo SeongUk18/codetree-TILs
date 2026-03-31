e565baf31e088b8095488204dc025ae1a43ee40f
Seonguk Han <adulty22@gmail.com>
2026-03-31 14:17:16 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260331/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.py
+++ 260331/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.py
@@ -2,22 +2,23 @@ n = int(input())
 sequence = list(map(int, input().split()))
 
 # Please write your code here.
-dp = [1] * n
+up_dp = [0] * n
+down_dp = [0] * n
 
 for i in range(n):
-    count = 0
-    flag = 'same'
+    up_dp[i] = 1
+    for j in range(i):
+        if sequence[j] < sequence[i]:
+            up_dp[i] = max(up_dp[i], up_dp[j] + 1)
 
+for i in range(n - 1, -1, -1):
+    down_dp[i] = 1
     for j in range(i + 1, n):
-        if sequence[i] > sequence[j] and flag != 'up':
-            count += 1
-            flag = 'up'
+        if sequence[j] < sequence[i]:
+            down_dp[i] = max(down_dp[i], down_dp[j] + 1)
 
-        elif sequence[i] < sequence[j] and flag != 'down':
-            count += 1
-            flag = 'down'
-
-        if count == 2:
-            dp[j] = max(dp[i] + j - i, j - i)
+dp = [0] * n
+for i in range(n):
+    dp[i] = up_dp[i] + down_dp[i] - 1
 
 print(max(dp))
